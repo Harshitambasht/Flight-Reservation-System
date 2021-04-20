3e30f68797d6bfbe998b443e68f4d240fac3b583
Harshit Ambasht <51776631+Harshitambasht@users.noreply.github.com>
2021-04-20 23:44:11 +0530

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    private Object RegularTicket, TouristTicket;
+    private Object ticket;
+
+    public void main(String[] args) {
+        public static void printTicketDetails = 0 (Ticket ticket);
+        Flight flightNumber = new Flight("AIR2548" , "IGI" , 300 , 250);
+
+        RegularTicket regularTicket = new RegularTicket("GUHHGGF" , "Delhi" ,
+                "Agra" , "Air India" , "21/04/2021 14:20" ,
+                "21/04/202118:25" , "Suresh");
+
+        TouristTicket touristTicket = new TouristTicket("duubr" , "Delhi" ,
+                "Agra" , "Air India" , "21/04/2002114:20" ,
+                "21/04/2021 18:25" , "Ram");
+
+        System.out.println(printTicketDetails);
+        System.out.println(printTicketDetails);
+
+    }
+}
