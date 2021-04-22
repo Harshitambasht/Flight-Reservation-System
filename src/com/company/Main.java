4499dee959ceb843f512f0f9d0e65306c2a3a0b7
Harshit Ambasht <51776631+Harshitambasht@users.noreply.github.com>
2021-04-22 14:24:30 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,19 @@
 package com.company;
 
 public class Main {
-        public static void main(String[] args) {
+    private static String pnr;
+
+    public static void main(String[] args) {
             Flight flight = new Flight();
             flight.flightNumber = "2445";
             flight.airline= "AIR INDIA";
             System.out.println(flight.flightNumber);
             System.out.println(flight.airline);
 
-
+            Ticket ticket=new RegularTicket(pnr);
+            printTicketDetails(ticket);
         }
+
+    private static void printTicketDetails(Ticket ticket) {
     }
+}
